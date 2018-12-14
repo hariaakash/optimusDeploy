73376826bfaeba3dc1e4e7d78d0022eef11fbd82
Hari Narayanan <smgdark@gmail.com>
2018-12-14 18:51:52 +0530

    added support for time parsing using moment

--- src/helpers/formatStats.js
+++ src/helpers/formatStats.js
@@ -1,12 +1,14 @@
+const moment = require('moment');
+
 const formatStats = (stats) => {
     let cpuDelta = stats.cpu_stats.cpu_usage.total_usage - stats.precpu_stats.cpu_usage.total_usage,
         systemDelta = stats.cpu_stats.system_cpu_usage - stats.precpu_stats.system_cpu_usage,
         cpu = (cpuDelta / systemDelta * 100) / stats.cpu_stats.online_cpus,
         ram = stats.memory_stats.usage / stats.memory_stats.limit * 100;
     return {
-        ram: ram >= 0 ? ram.toFixed(2) : -1,
-        cpu: cpu >= 0 ? cpu.toFixed(2) : -1,
-        read: stats.read,
+        ram: parseFloat(ram >= 0 ? ram.toFixed(2) : -1),
+        cpu: parseFloat(cpu >= 0 ? cpu.toFixed(2) : -1),
+        read: moment(stats.read).valueOf(),
     };
 };
 
