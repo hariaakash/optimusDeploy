34620ea4bd27e68097601acfbd0b203065fbb444
aarsh patel <aarshonu@gmail.com>
2019-03-28 18:21:38 +0530

    All services running on the planned stack. stack.elastic integrated with traefik.

--- stack-start.sh
+++ stack-start.sh
@@ -1,8 +1,12 @@
+# Start Utils Stack
+docker stack deploy -c stack.dev.utils.yml utils
+
+#Start Elastic Stack
+docker stack deploy -c stack.elastic.yml elastic
+
 # Build docker image for services.
 docker-compose -f docker-compose.dev.yml build
 
-# Start Utils Stack
-docker stack deploy -c stack.dev.utils.yml utils
 
 # Make build images available on private registry.
 services=("api" "orchestrator" "user" "mailer" "container")
@@ -10,12 +14,9 @@ MANAGER_IP=$(docker node inspect self --format '{{ .Status.Addr  }}')
 for service in "${services[@]}"
 do
     service="optimusdeploy_$service:dev"
-    docker tag $service registry.local:5000/$service
-    docker push registry.local:5000/$service
+    docker tag $service registry.local/$service
+    docker push registry.local/$service
 done
 
 # Start Services Stack
 docker stack deploy -c stack.dev.services.yml services
-
-#Start Elastic Stack
-docker stack deploy -c stack.elastic.yml services
